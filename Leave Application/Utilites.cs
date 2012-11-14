e4e601c90e799bbcf08eac2b35349c1746d059bd
unknown <praveen@TILLID-PC1.tillid.com>
2012-11-14 19:07:11 +0530

    Intial Upload

---
+++ Leave Application/Utilites.cs
@@ -0,0 +1,40 @@
+﻿namespace Leave_Application
+{
+    internal class Utilities
+    {
+        public static string Department = "Department";
+        public static string Designation = "Designation";
+        public static string EmployeeType = "Employee Type";
+        public static string HolidayList = "Holiday List";
+        public static string EmployeeLeaves = "Employee Leaves";
+        public static string EmployeeScreen = "Employee Screen";
+        public static string ReportingTo = "Reporting Members";
+        public static string EmployeeId = "Title";
+        public static string EmployeeName = "Employee Name";
+        public static string Manager = "Manager";
+        public static string FirstName = "First Name";
+        public static string LastName = "Last Name";
+        public static string DateofJoin = "Date of Join";
+
+        //public static string EmployeeType = "Employee Type";
+        public static string DOB = "DOB";
+
+        public static string Email = "Email";
+        public static string Mobile = "Mobile";
+        public static string LeaveRequest = "Leave Request";
+
+        public static string Status = "Status";
+
+        public static string LeaveType = "Leave Type";
+        public static string StartingDate = "Starting Date";
+        public static string EndingDate = "Ending Date";
+        public static string LeaveDays = "Leave Days";
+        public static string Remarks = "Remarks";
+        public static string CurrentYear = "CurrentYear";
+        public static string Year = "Year";
+        public static string UpdatingMonth = "UpdatingMonth";
+        public static string UpdatedDate = "UpdatedDate";
+        public static string Financialstartmonth = "Financial start month";
+        public static string CompoffList = "Compoff Details";
+    }
+}
